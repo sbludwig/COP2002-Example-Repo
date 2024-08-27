4baeb152852f2b633fc96ad25d81d0b1190b72d0
sbludwig <37007242@sfcollege.edu>
2024-08-26 22:07:30 -0400

    Update githubpractice.py

--- githubpractice.py
+++ githubpractice.py
@@ -4,4 +4,4 @@
 print("COP2002 Fall 2024")
 COP2002 Fall 2024
 
-+print("I'm understanding how to use GitHub to keep track of changes!")
+print("I'm understanding how to use GitHub to keep track of changes!")
